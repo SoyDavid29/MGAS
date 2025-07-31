fae1f4d77909021903307e3bff748a4d23f81b80
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-07-31 14:59:14 -0600

    new section created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Sidebar from "./Sidebar"
 import Hero from "./Hero"
 import Menu from "./Menu"
+import Section from "./Section";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false); // shared state with toggle menu
@@ -12,6 +13,25 @@ function App() {
     <Sidebar setMenuOpen={setMenuOpen} menuOpen={menuOpen} />
     <Menu menuOpen={menuOpen} />
     <Hero />
+
+    <div className="info">
+      <div className="malala">
+        <img className="malala-photo" src="/images/imagen1.png" alt="malala poster" />
+      </div>
+
+      <div className="info-fundacion">
+        <h2>Apoyando a más de 1,234
+          familias, por: Más de 2700
+          pacientes beneficiados.
+        </h2>
+        <h1>FUNDACION MGAS.AC</h1>
+
+        <img className="logo-asociasion" src="/images/imagen6.png" alt="logo asociacion" />
+      </div>
+    </div>
+
+    <Section/>
+    
   </>
   );
 }
---
+++ src/Section.jsx
@@ -0,0 +1,50 @@
+export default function Section() {
+    return (
+        <section className="institucion">
+
+            <h1>NUESTRA INSTITUCION</h1>
+
+            <div className="institution-galery">
+
+                <div className="post">
+                    <img src="/images/imagen7.png" alt="imagen" />
+                    <h3>MISIÓN</h3>
+                    <p>Apoyar a niños en situacion de pobreza extrema,
+                        marginación, población indígena y escasos recursos
+                        con parálisis cerebral, displegia espástica, cuadríplegía
+                        espástica o alguna espasticidad para que puedan ser valorados y
+                        en su caso intervenidos.
+                    </p>
+
+                </div>
+
+                <div className="post">
+                    <img src="/images/imagen8.png" alt="imagen" />
+                    <h3>VISIÓN</h3>
+                    <p>
+                        Mejorar la calidad de vida de niños y niñas con Parálisis Cerebral
+                        espástica y sus familias. Fibrotomía
+                        Gradual Muscular, logrando llevar las Jornadas de Cirugías a
+                        diferentes estados de la República Mexicana y países de
+                        Latinoamérica.
+                    </p>
+                </div>
+
+                <div className="post">
+                    <img src="/images/imagen9.png" alt="imagen" />
+                    <h3>Valores</h3>
+                    <p>
+                        •	AMOR •TEMPLANZA •ESPERANZA
+                        •	SOLIDARIDAD •ESFUERZO •DISCIPLINA
+                        •	ALEGRIA •RESPETO •TOLERANCIA
+                        •	RESPONSABILIDAD •FE •FAMILIA
+                        •	VOLUNTARIADO •HONESTIDAD
+                    </p>
+                </div>
+
+            </div>
+
+        </section>
+    );
+
+}
